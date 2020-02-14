ad8ef56347c829cb8136c2883a0b89b3b2b85186
chenxuewei <chen.xuewei@yilutong.com>
2020-02-14 11:44:35 +0800

    连接篇以开发完成

--- src/page/product/storeManage/linkOne.js
+++ src/page/product/storeManage/linkOne.js
@@ -2,7 +2,7 @@ import React, {
   Component
 } from 'react'
 import Author from '../../../component/author.js';
-import BlockQuote from '../../../component/blockquote.js';
+// import BlockQuote from '../../../component/blockquote.js';
 import Preface from '../../../component/Even/preface.js';
 import * as data from './pageData.js';
 // import commonFn from '../../../config/common.js';
@@ -29,21 +29,32 @@ class PagePart extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      css:{
-        'box':[
-          {selector: '.box1', key: 'display', value: 'none', start: '//我是第一个注释', end: '/#/我是第二个注释'},
-          {selector: '.box1',key: 'font-size', value: '12px', start: '字体属性', end: '/字体值/'},
-        ]
-      },
-      html: `
-<--!html笔记 -->
-<div id="title">
-  笔记开发文档
-</div>`
+      html: '',
     };
   }
   componentWillMount() {}
-  componentDidMount() {}
+  componentDidMount() {
+    this.createdHtml()
+  }
+  createdHtml() {
+    let html = data.linksName.map((item, index) => {
+      return(
+        <React.Fragment key={index}>
+          <h3 className={item.categoryEnglish}>{item.category}</h3>
+          {data.links[index].map((linksItem, linkIndex) => {
+            return(
+              <li className="li" key={linkIndex}>
+                <span className="circular"></span>
+                <a rel="noopener noreferrer" href={linksItem.href} target="_blank">{linksItem.title}</a>
+                <span className="notes">{`(${linksItem.notes})`}</span>
+              </li>
+            )
+          })}
+        </React.Fragment>
+      )
+    })
+    this.setState({html})
+  }
   componentWillReceiveProps() {
     //props更新
     //约1s 调用一次
@@ -62,54 +73,7 @@ class PagePart extends Component {
           </div>
           <Author createdTime="2020年2月2日"/>
           <Preface content={data.preface}/>
-          
-          <p>第一个博客，想了很久css代码块该怎么展示才能让读者看的更舒服美观，
-            也借鉴了阮一峰大神的博客样式表，才有了现在的灵感，
-            自己封装的组件感觉还是比较繁琐，先对付着用吧，
-            后面如果想到简化版的在去更新，还是要给自己写一个关于样式表组件的使用说明，
-            一是为了怕自己鱼的记忆，还要翻原来的代码一点点的看，
-            二是博客确实需要花费很大的时间和经历，所以也是留给以后的自己在续写博客的时候方便一些。
-            确实随着年龄的增长记忆是越来越差了。
-          </p>
-          {/* <p>第一篇文章 <strong>这是一个加粗字体</strong>，<a href="https://www.baidu.com/" target="_blank">这是一个连接</a>都挺好的</p> */}
-          <div id="pre">
-            <pre>
-              <div>
-                <pre>
-                  {`//这个是注释
-                  `}
-                </pre>
-              </div>
-              {`.list {
-                font-size: 12px; /伪装/
-                background: red;
-              }
-              `}
-            </pre>
-
-            <pre>
-            <span id="nihao" style={{"color": "green"}}>{`{`}</span>
-            </pre>
-          </div>
-      
-          <br/>
-          nihao
-          <br/>
-          nihao
-          <br/>
-          <div id="title">一、css连接</div>
-          <BlockQuote structureName="css" content={this.state.css.box}/>
-          nihao
-          <br/>
-          nihao
-          <BlockQuote structureName="" content={this.state.html}/>
-          <br/>
-          nihao
-          <br/>
-          nihao
-          <br/>
-          nihao
-          <br/>
+          {this.state.html}
         </div>
       </div>
     );
--- src/page/product/storeManage/pageData.js
+++ src/page/product/storeManage/pageData.js
@@ -4,7 +4,57 @@ let preface = `
 连浏览器里收藏了很多关于技术的文章，种类的增多也无法满足自己的需求，
 所以有了下面关于外联的笔记，分类功能等方便以后的查看和管理。
 `
+let linksName = [
+  {category: '文档类', categoryEnglish: 'document'},
+  {category: '框架类', categoryEnglish: 'frame'},
+  {category: '工具类', categoryEnglish: 'tool'},
+  {category: '学习类', categoryEnglish: 'study'},
+  {category: '铭人类', categoryEnglish: 'literature'},
+  {category: '工作类', categoryEnglish: 'work'},
+  {category: '其他类', categoryEnglish: 'other'},
+]
+
+// 文档类链接
+let links = [
+  //文档类
+  [
+    {title: '百度链接', href:"https://www.baidu.com/", notes:"我是注释"},
+    {title: '其他链接', href:"https://www.baidu.com/", notes:"我是注释"},
+  ],
+  //框架类
+  [
+    {title: '百度链接', href:"https://www.baidu.com/", notes:"我是注释"},
+    {title: '其他链接', href:"https://www.baidu.com/", notes:"我是注释"},
+  ],
+  //工具类
+  [
+    {title: '百度链接', href:"https://www.baidu.com/", notes:"我是注释"},
+    {title: '其他链接', href:"https://www.baidu.com/", notes:"我是注释"},
+  ],
+  //铭人类
+  [
+    {title: '百度链接', href:"https://www.baidu.com/", notes:"我是注释"},
+    {title: '其他链接', href:"https://www.baidu.com/", notes:"我是注释"},
+  ],
+  //工作类
+  [
+    {title: '百度链接', href:"https://www.baidu.com/", notes:"我是注释"},
+    {title: '其他链接', href:"https://www.baidu.com/", notes:"我是注释"},
+  ],
+  //其他类
+  [
+    {title: '百度链接', href:"https://www.baidu.com/", notes:"我是注释"},
+    {title: '其他链接', href:"https://www.baidu.com/", notes:"我是注释"},
+  ],
+  //文档类
+  [
+    {title: '百度链接', href:"https://www.baidu.com/", notes:"我是注释"},
+    {title: '其他链接', href:"https://www.baidu.com/", notes:"我是注释"},
+  ],
+]
 
 export {
   preface,
+  links,
+  linksName,
 }
